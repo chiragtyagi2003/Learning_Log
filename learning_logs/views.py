048fdeab9c9db76c5b0e3e624633d0ca4ff71fd6
chiragtyagi <tyagichirag06@gmail.com>
2022-07-14 22:37:34 +0530

    the topics page view function

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 
 def index(request):
     """The home page for Learning_Logs."""
     return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """The topics page."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topic}
+
+    return render(request, 'learning_logs/topics.html', context)
