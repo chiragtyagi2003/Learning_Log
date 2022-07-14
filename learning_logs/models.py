be36df965ab1c3a791efd63805d1f268bb6065a0
chiragtyagi <tyagichirag06@gmail.com>
2022-07-14 20:47:34 +0530

    created the model for storing the entries associated with a topic

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,7 +1,9 @@
+from operator import truediv
 from django.db import models
 
 # Create your models here.
 
+# Model to store a topic
 class Topic(models.Model):
     """A topic the user is learning about"""
     text = models.CharField(max_length=200)
@@ -10,3 +12,17 @@ class Topic(models.Model):
     def __str__(self):
         """Return a string representation of the model"""
         return self.text 
+
+# Model to store an entry associated with a topic
+class Entry(models.Model):
+    """Information about the topic, currently learning"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Represent a string representation of the model"""
+        return f"{self.text[:50]}..."
