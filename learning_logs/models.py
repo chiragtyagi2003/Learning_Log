6fa6065928219153076133b6be9464aee8a9504d
chiragtyagi <tyagichirag06@gmail.com>
2022-07-14 20:59:11 +0530

    spelling mistake in date_added in model Topic

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -7,7 +7,7 @@ from django.db import models
 class Topic(models.Model):
     """A topic the user is learning about"""
     text = models.CharField(max_length=200)
-    data_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """Return a string representation of the model"""
