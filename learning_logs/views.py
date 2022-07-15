798f75a2412705fe0b3b900adc5b0318de360165
chiragtyagi <tyagichirag06@gmail.com>
2022-07-15 19:44:55 +0530

    view function for the new_entry form

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
@@ -50,3 +50,59 @@ def new_topic(request):
     #display a blank or invalid form
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """Adds an entry to a particular topic"""
+
+    #retreive the topic and store it in a var using the topic id
+    topic = Topic.Objects.get(id=topic_id)
+
+    #if the request method is not POST
+    if request.method != 'POST':
+
+        #make a blank  entry form to make an entry
+        #to the specified topic
+        form = EntryForm()
+
+    #else if the request is POST
+    else:
+        
+        #make a entry form and store the entry data
+        #which is stored in the request.POST
+        form = EntryForm(data=request.POST)
+
+        #check if the form meets the requirements
+        if form.is_valid():
+
+            #save the new entry in a variable but don't
+            #save the entry in DB yet, because first we need
+            #to associate this entry with the topic
+            new_entry = form.save(commit=False)
+            
+            #associate the new_entry to the topic
+            
+            #you can access its topic attribute now beacause
+            #we have saved the form into it. 
+            new_entry.topic = topic
+
+            #save this new entry to the DB
+            new_entry.save()
+
+            #after saving the entry to the topic
+            #redirect the user to that particular
+            #Topic's page
+            return redirect ('learning_logs:topic', topic_id=topic_id)
+
+
+    #Display the blank form or invalid form
+    context = {'topic':topic, 'form':form}
+    return render(request, 'learning_logs/new_entry.html', context)
+
+
+
+
+
+
+
+
