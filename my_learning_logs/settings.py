a1226c6b7aa2e59ff1d66d968bcfff642254e310
chiragtyagi <tyagichirag06@gmail.com>
2022-08-03 13:11:07 +0530

    bootstrap4 to style the app

--- my_learning_logs/settings.py
+++ my_learning_logs/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'learning_logs',
     'users',
 
+    #third party apps
+    'bootstrap4',
+
     #default django apps
     'django.contrib.admin',
     'django.contrib.auth',
